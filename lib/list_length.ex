57ca0a20e3bf8ce75de4384f66415492a5194062
Thiago Oliveira Marçal <thiagoomarcal@protonmail.com>
2022-06-06 18:57:53 -0300

    Ignite Trilha Elixir - Desafio 01 Utilizando recursão

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,15 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: get_list_length(list, 0)
+
+  defp get_list_length([], acc), do: acc
+
+  defp get_list_length([_head | tail], acc) do
+    acc = acc + 1
+
+    if length(tail) == 0 do
+      get_list_length(tail, acc)
+    end
+
+    get_list_length(tail, acc)
   end
 end
